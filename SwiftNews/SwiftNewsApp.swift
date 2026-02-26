755a47b9d0174cab15a670874d1d22d879510456
Vladyslav <nadaxovkiy@gmail.com>
2026-02-26 14:14:47 +0200

    Initial Commit

---
+++ SwiftNews/SwiftNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftNewsApp.swift
+//  SwiftNews
+//
+//  Created by Влад Надаховский on 26.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct SwiftNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
